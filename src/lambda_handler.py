6620eb5600ce0848f7c0c7c5b508df90b14752bd
David Odesomi <davidodesomi@gmail.com>
2026-04-30 08:05:16 +0100

    Updated lambda_handler.py with new imports (dynamodb_client, s3_client)

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -1,8 +1,12 @@
 import json
+import dynamodb_client
+import s3_client
 
 def lambda_handler(event, context):
     body = event["body"]
     body = json.loads(body)
     session_id = body["session_id"]
-    message = body["message"]
-    history = get_chat_history["session_id"]
+    context = s3_client.message
+    history = dynamodb_client.get_chat_history(session_id)
+
+    
