ed1f36075d674743d2d3f15bb483545554e8bd16
David Odesomi <davidodesomi@gmail.com>
2026-05-01 13:12:08 +0100

    Created get_chat_history function in dynamodb_client.

--- src/dynamodb_client.py
+++ src/dynamodb_client.py
@@ -1,4 +1,14 @@
 import boto3
+from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 table = dynamodb.Table('ChatHistory')
+
+
+
+def get_chat_history(session_id):
+    history = table.query(
+        KeyConditionExpression=Key('session_id').eq(session_id)
+    )
+
+    return history['Items']
